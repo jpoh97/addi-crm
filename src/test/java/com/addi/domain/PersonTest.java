3145e421bfa8fd6b8eca92e5c1001f604c0b3e84
jpoh97 <juan.pablo@corp.cision.com>
2021-02-03 23:13:06 -0500

    Added failing test of 2 people with different data

--- src/test/java/com/addi/domain/PersonTest.java
+++ src/test/java/com/addi/domain/PersonTest.java
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @DisplayName("Agregar contratista")
 final class PersonTest {
 
     @Test
     @DisplayName("2 people with the same data must be equal")
-    void testTwoPeopleAreTheSame() {
+    void testTwoPeopleEqualData() {
         Person person1 = new PersonBuilder().build();
         Person person2 = new PersonBuilder().build();
 
         assertEquals(person1, person2);
+    }
+
+    @Test
+    @DisplayName("2 people with the different data must be not equal")
+    void testTwoPeopleNotEqualData() {
+        Person person1 = new PersonBuilder().build();
+        Person person2 = new PersonBuilder().build();
+
+        assertNotEquals(person1, person2);
     }
 }
